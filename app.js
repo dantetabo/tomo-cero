af412db79ef216df75e70ebdae9ae2608944e5fd
Dante Tabo <dantetabo@gmail.com>
2023-06-22 01:39:42 -0300

    preubas

--- app.js
+++ app.js
@@ -37,14 +37,18 @@ if (ingresoUsuario) {
 };
 */
 
-const manga = {
+/* const manga = {
   berserk: [
     {id: 1, nombre:"Berserk #1", precio: 2000},
     {id: 2, nombre:"Berserk #2", precio: 2500},
     {id: 3, nombre:"Berserk #3", precio: 3200},
     {id: 4, nombre:"Berserk #4", precio: 2000},
     {id: 5, nombre:"Berserk #5", precio: 2500},
-    {id: 6, nombre:"Berserk #6", precio: 1500}
+    {id: 6, nombre:"Berserk #6", precio: 1500},
+    {id: 7, nombre:"Berserk #7", precio: 3200},
+    {id: 8, nombre:"Berserk #8", precio: 2000},
+    {id: 9, nombre:"Berserk #9", precio: 2500},
+    {id: 10, nombre:"Berserk #10", precio: 1500}
   ],
   vinland: [
     {id: 1, nombre:"Vinland #1", precio: 2000},
@@ -87,7 +91,7 @@ switch(criterioBusqueda){
  */
 
 
-function filtrar(arr, filtro, param) {
+/* function filtrar(arr, filtro, param) {
   return arr.filter((el) => {
     if (param === "id") {
       return el.id == filtro;
@@ -100,4 +104,61 @@ function filtrar(arr, filtro, param) {
 }
 
 
-console.log(filtrar(manga[mangaElegido], filtrador, criterioBusqueda));
+console.log(filtrar(manga[mangaElegido], filtrador, criterioBusqueda)); */ 
+
+
+const manga = {
+  berserk: [
+    {id: 1, nombre: "Berserk #1", precio: 2000},
+    {id: 2, nombre: "Berserk #2", precio: 2500},
+    {id: 3, nombre: "Berserk #3", precio: 3200},
+    {id: 4, nombre: "Berserk #4", precio: 2000},
+    {id: 5, nombre: "Berserk #5", precio: 2500},
+    {id: 6, nombre: "Berserk #6", precio: 1500},
+    {id: 7, nombre: "Berserk #7", precio: 3200},
+    {id: 8, nombre: "Berserk #8", precio: 2000},
+    {id: 9, nombre: "Berserk #9", precio: 2500},
+    {id: 10, nombre: "Berserk #10", precio: 1500}
+  ]
+};
+
+
+
+const carritoItems = [];
+
+
+const buyButtons = document.querySelectorAll('.btnCompra');
+
+buyButtons.forEach(function(button) {
+button.addEventListener('click', function() {
+  const id = parseInt(button.getAttribute('data-id'));
+
+  const mangaItem = manga.berserk.find(function(item) {
+    return item.id === id;
+  });
+
+  if (mangaItem) {
+
+    carritoItems.push(mangaItem);
+
+    actualizarCarritoHTML();
+  }
+});
+});
+
+
+function actualizarCarritoHTML() {
+
+const carritoElement = document.getElementById('carrito');
+
+carritoElement.innerHTML = '';
+
+
+carritoItems.forEach(function(item) {
+
+  const itemElement = document.createElement('p');
+  itemElement.textContent = `${item.nombre} - Precio: $${item.precio}`;
+
+  carritoElement.appendChild(itemElement);
+});
+}
